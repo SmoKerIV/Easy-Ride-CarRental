f016dcabe6528b64828e96b7c786ceb36f829ed2
Baker Alazzawi <Baker.alazzawi0@gmail.com>
2024-01-16 17:27:18 +0300

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,26 @@
 import Container from './Components/Container/Container'
 import Header from './Components/Header/Header';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <>
       <Container>
-        <Header/>
-        <div className="hero">
-          <h1>hello</h1>
+        <Header />
+        <div className={styles.hero}>
+          <div className={styles.blue}></div>
+          <h1 className={styles.herotitle}>
+            Car Rental
+            <br /> For Every
+            <br /> Occasion
+          </h1>
+          <p className={styles.herodescription}>
+            <b>
+              Easy<span>Ride</span>
+            </b>{" "}
+            Car Rental is an online car rental service that provides a wide
+            selection of cars for rental at an affordable price.
+          </p>
         </div>
       </Container>
     </>

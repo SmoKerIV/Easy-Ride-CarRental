0098668104ca6b15f36c64c838ba3ce4940f1e34
99aliyousif99 <128894509+99aliyousif99@users.noreply.github.com>
2024-01-24 22:20:55 +0300

    commit

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+import { compare } from "bcryptjs";
+import { NextResponse } from "next/server";
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-export const authOptions = {
-  // Configure one or more authentication providers
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const prisma = new PrismaClient();
+
+const authOptions = {
+
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
+
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {},
+        password: {},
+      },
+
+      async authorize(credentials,req) {
+        const user = await prisma.users.findUnique({
+          where: { email : credentials.email },
+        });
+
+        if (user) {
+          const isValid = await compare(credentials.password, user.password);
+
+          if (isValid) {
+            return NextResponse.json({ 
+              success: true,
+              id: user.id,
+              name: user.email });
+          } else {
+            return NextResponse.json({ success: false, message: "Invalid password" });
+          }
+        } 
+      },
     }),
-    // ...add more providers here
   ],
 };
-export const handler = NextAuth(authOptions);
 
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };

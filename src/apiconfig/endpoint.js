5a94c3fec577af82f6f8fba85c17121314f886a2
Baker Alazzawi <Baker.alazzawi0@gmail.com>
2024-01-29 23:52:35 +0300

    Update endpoint.js

--- src/apiconfig/endpoint.js
+++ src/apiconfig/endpoint.js
@@ -3,4 +3,4 @@ export const CARS_URL2 = "http://localhost:3000/api/cars";
 export const BRAND_URL2 = "http://localhost:3000/api/brands";
 export const BASE_URL = "https://easy-ride-car-rental.vercel.app/api";
 export const CARS_URL = "https://easy-ride-car-rental.vercel.app/api/cars";
-export const BRAND_URL = "https://easy-ride-car-rental.vercel.app/api/categories";
+export const BRAND_URL = "https://easy-ride-car-rental.vercel.app/api/brands";
